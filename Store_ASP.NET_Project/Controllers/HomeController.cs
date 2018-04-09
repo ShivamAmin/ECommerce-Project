a29117f41fac55b8bdde91447d8e4c852442d3df
Shivam Amin <xShivam.Amin@gmail.com>
2018-04-09 12:31:04 -0400

    Working out the login system
    Working on the HomeController, and changed the default layout

---
+++ Store_ASP.NET_Project/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store_ASP.NET_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(loggedIn)
+            {
+                return View();
+            } else
+            {
+                RedirectToAction("Login");
+            }
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View()
+        }
+        [HttpPost]
+        public ActionResult Login(String username, String password)
+        {
+            
+        }
+    }
+}
