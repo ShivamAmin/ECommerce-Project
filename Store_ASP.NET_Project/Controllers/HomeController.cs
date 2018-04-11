5f91d682e3015ba10823fa2cceb39d2d2e2c0c6c
Shivam Amin <xShivam.Amin@gmail.com>
2018-04-10 23:02:21 -0400

    Login mechanism complete
    All pages will now redirect to login page if user not logged in. If user is logged in, they will be able to browse like normal. Added logout page. Updated layout to display correct developer names.

---
+++ Store_ASP.NET_Project/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store_ASP.NET_Project.Models;
+
+namespace Store_ASP.NET_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private StoreDatabase db = new StoreDatabase();
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(Session["loggedIn"] == null || (bool)Session["loggedIn"] == false)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            User ActualUser = db.Users.SingleOrDefault(u => u.Username == user.Username);
+            if(ActualUser != null)
+            {
+                if(ActualUser.Password == user.Password)
+                {
+                    Session["loggedIn"] = true;
+                    return RedirectToAction("Index", "Products", new { area = "" });
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session["loggedIn"] = false;
+            return RedirectToAction("Login");
+        }
+    }
+}
