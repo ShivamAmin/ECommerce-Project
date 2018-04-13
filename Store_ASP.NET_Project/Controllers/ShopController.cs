a92fa6a77dea0c05cccfed0ead75fe960d96f55f
Shivam Amin <xShivam.Amin@gmail.com>
2018-04-13 02:10:02 -0400

    Started work on the shop
    Started to layout how it looks. Still working out css/bootstrap 3.0.0 quirks.

---
+++ Store_ASP.NET_Project/Controllers/ShopController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store_ASP.NET_Project.Models;
+
+namespace Store_ASP.NET_Project.Controllers
+{
+    public class ShopController : Controller
+    {
+        private StoreDatabase db = new StoreDatabase();
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
