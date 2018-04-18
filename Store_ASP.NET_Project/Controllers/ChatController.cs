efa09462658856a6ca1000e079a55bb09ab51117
stamatopoulos <38237599+stamatopoulos@users.noreply.github.com>
2018-04-18 17:55:32 -0400

    Add files via upload

---
+++ Store_ASP.NET_Project/Controllers/ChatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store_ASP.NET_Project.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
