cef580ece240b8fbf702d8ea2d140a9dba14092e
Salman Rainullah Mir <salmanrain97@gmail.com>
2018-04-21 21:14:40 -0400

    Half Committed changes

---
+++ Store_ASP.NET_Project/Controllers/ReviewsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Store_ASP.NET_Project.Models;
+
+namespace Store_ASP.NET_Project.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private StoreDatabase db = new StoreDatabase();
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            //add lists by category in viewBag [refer to petshop lab 7]
+            ViewBag.productCategory = new SelectList(db.Reviews, "Category", "Category");
+            List<Review> reviewTblList = db.Reviews.Where(item => item.Category == "Electronics").ToList();
+            return View(reviewTblList);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string productCategory)
+        {
+
+            List<Review> reviewTblList = db.Reviews.Where(item => item.Category == productCategory).ToList();
+            return View(reviewTblList);
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Category,Subject,Comment,UserId")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(review);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Category,Subject,Comment,UserId")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Store_ASP.NET_Project/Models/StoreContext.Designer.cs
+++ Store_ASP.NET_Project/Models/StoreContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\xShiv\Documents\ECommerce-Project\Store_ASP.NET_Project\Models\StoreContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\User\Desktop\Semester 4 Computer Programming\ASP.Net\ECommerce-Project\Store_ASP.NET_Project\Models\StoreContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
